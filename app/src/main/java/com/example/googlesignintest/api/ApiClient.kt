e76234f513c67e8602e30b0133339e3956b403a3
William Riser <riser.w@northeastern.edu>
2025-01-19 10:53:12 -0500

    localhost fix

--- app/src/main/java/com/example/googlesignintest/api/ApiClient.kt
+++ app/src/main/java/com/example/googlesignintest/api/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://<YOUR_BACKEND_URL>/"
+    private const val BASE_URL = "http://10.0.2.2:5001/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
